12213e0a43ccd23cfd11a38ebf269fa86b258858
Dylan Storm Johnson <dylanstormjohnson@comcast.net>
2023-04-18 02:13:06 -0500

    Final Draft

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # weather-updater
 Weather updater website to track weather patterns across the world and your previous inquiries.
+
+Image
+
+Here is an image of the completed website's Homepage:
+![The Homepage in full, showing its many elements, including its buttons, navigation, and title.](./assets/images/Weather_Checker_Full_Homepage_Screenshot.pdf)
+
+Description
+    - what
+    - why
+    - how
+
+    - This website was created to serve as a quick reference for the weather and forecast in specific locations..
+
+    - It is meant to be a quick way of determining the current weather and five-day forecast at a given location when searched by the user.
+
+    - This website was created using HTML, CSS, Javascript, Bootstrap, and JQuery formatting to provide ease-of-use and quick access.
+
+    - This website solves the problem of not knowing the weather and forecast in an area.
+
+    --- As per the assignment:
+
+    -- The HTML, CSS, and Javascript coding has been built from scratch to form the elements of the website.
+
+    - This was done using elements such as: functions, variables, for loops, intervals, if statements, DOM traversal, DOM manipulation, localstorage.getItem/localstorage.setItem, eventListeners, and fetch requests, API calls, JSON.stringify(), and JSON.parse().
+
+    - A search button, input bar, and a clear button are provided for ease-of-use.
+
+    - Once a valid location is searched, the current weather and five-day forecast are provided, as well as a button to make prior searches quicker.
+
+    - Once an invalid location is searched, an alert box will appear stating that only valid locations can be searched.
+
+    - Once the clear button is clicked, the other buttons of all prior searches are removed and the site is reloaded.
+
+    This website was coded by Dylan Storm Johnson.
---
+++ assets/js/script.js
@@ -0,0 +1,139 @@
+const apiKeyGeo = "32f3290ba034d4bcd6196fdb06151ab8";
+const apiKeyWeather = "03c7516ad8b972a061cba33872462d14";
+let searchedLocation = "";
+let iconImgUrl = "http://openweathermap.org/img/w/";
+// localStorage.clear();
+let priorSearches = JSON.parse(localStorage.getItem("searches")) || [];
+
+$(document).ready(function () {
+  const loadStorage = () => {
+    for (let i = 0; i < priorSearches.length; i++) {
+      // console.log("prior search term in loading");
+      // console.log(priorSearches[i]);
+      let newButton = $("<button>");
+      newButton.text(priorSearches[i]);
+      newButton.attr(
+        "class",
+        "col-sm-12 btn btn-info btn-outline-primary priorSearchBtn"
+      );
+      $("#newButtonsHolder").append(newButton);
+    }
+  };
+
+  loadStorage();
+
+  $("#searchBtn").click(function (event) {
+    searchedLocation = $("#city").val();
+    checkLocation(searchedLocation);
+  });
+
+  $(".priorSearchBtn").click(function (event) {
+    searchedLocation = $(this).text();
+    console.log(searchedLocation);
+    checkLocation(searchedLocation);
+  });
+
+  $("#clearBtn").click(function (event) {
+    console.log("Clearing");
+    localStorage.setItem("searches", JSON.stringify([]));
+    location.reload();
+  });
+  const checkLocation = (cityToSearch) => {
+    fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${cityToSearch}&appid=${apiKeyGeo}`
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        if (data.length === 0) {
+          alert("you must search for a valid city");
+          return;
+        }
+        let locationLat = data[0].lat;
+        addToStorage(data[0].name);
+        let locationLon = data[0].lon;
+        checkForecast(locationLat, locationLon);
+        $("#locationName").text(searchedLocation);
+      });
+  };
+
+  const checkForecast = (lat, lon) => {
+    fetch(
+      `https://api.agromonitoring.com/agro/1.0/weather/forecast?lat=${lat}&lon=${lon}&appid=${apiKeyWeather}`
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        $("#temp").text("Temp: " + data[0].main.temp);
+        $("#wind").text("Wind: " + data[0].wind.speed);
+        $("#humidity").text("Humidity: " + data[0].main.humidity);
+
+        $("#day1Icon").attr(
+          "src",
+          iconImgUrl + data[0].weather[0].icon + ".png"
+        );
+        $("#day1Temp").text("Temp: " + data[0].main.temp);
+        $("#day1Wind").text("Wind: " + data[0].wind.speed);
+        $("#day1Humidity").text("Humidity: " + data[0].main.humidity);
+
+        $("#day2Icon").attr(
+          "src",
+          iconImgUrl + data[1].weather[0].icon + ".png"
+        );
+        $("#day2Temp").text("Temp: " + data[1].main.temp);
+        $("#day2Wind").text("Wind: " + data[1].wind.speed);
+        $("#day2Humidity").text("Humidity: " + data[1].main.humidity);
+
+        $("#day3Icon").attr(
+          "src",
+          iconImgUrl + data[2].weather[0].icon + ".png"
+        );
+        $("#day3Temp").text("Temp: " + data[2].main.temp);
+        $("#day3Wind").text("Wind: " + data[2].wind.speed);
+        $("#day3Humidity").text("Humidity: " + data[2].main.humidity);
+
+        $("#day4Icon").attr(
+          "src",
+          iconImgUrl + data[3].weather[0].icon + ".png"
+        );
+        $("#day4Temp").text("Temp: " + data[3].main.temp);
+        $("#day4Wind").text("Wind: " + data[3].wind.speed);
+        $("#day4Humidity").text("Humidity: " + data[3].main.humidity);
+
+        $("#day5Icon").attr(
+          "src",
+          iconImgUrl + data[4].weather[0].icon + ".png"
+        );
+        $("#day5Temp").text("Temp: " + data[4].main.temp);
+        $("#day5Wind").text("Wind: " + data[4].wind.speed);
+        $("#day5Humidity").text("Humidity: " + data[4].main.humidity);
+      });
+  };
+
+  const addToStorage = (whatToSave) => {
+    if (!priorSearches.includes(whatToSave)) {
+      priorSearches.push(whatToSave);
+      priorSearches.sort();
+      localStorage.setItem("searches", JSON.stringify(priorSearches));
+      makeButtons(whatToSave);
+    }
+  };
+
+  const makeButtons = (newValue) => {
+    let newButton = $("<button>");
+    newButton.text(newValue);
+    newButton.attr(
+      "class",
+      "col-sm-12 btn btn-info btn-outline-primary priorSearchBtn"
+    );
+    $("#newButtonsHolder").append(newButton);
+
+    $(".priorSearchBtn").click(function (event) {
+      searchedLocation = $(this).text();
+      console.log(searchedLocation);
+      checkLocation(searchedLocation);
+    });
+  };
+});
